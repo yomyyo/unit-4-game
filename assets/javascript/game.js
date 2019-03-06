50eb2b2b299d9c38c76a549a20d477df7d227480
Sammy Tang <samtang226@gmail.com>
2019-03-06 11:51:54 -0800

    added more comments for reading

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,30 @@
+//Global Variables
 var wins = 0;
 var losses = 0;
 var total = 0;
+
+//Display the initial score
 $("#totalScoreDiv").text(total);
 
-
+//function for the logic of the game
 function clickedGem(){
 
+    //initialize variables for game first playthrough
     var randomNum =  randomIntFromInterval(19, 120);
     var gemOneVal = Math.floor(Math.random() * 12) + 1;
     var gemTwoVal = Math.floor(Math.random() * 12) + 1;
     var gemThreeVal = Math.floor(Math.random() * 12) + 1;
     var gemFourVal = Math.floor(Math.random() * 12) + 1;
+
+    // displays the current number of wins, losses, and the current random number
     displayScore(randomNum);
 
-    
+    //When gemOne button is clicked, increase total, update the total display
     $('#gemOne').on("click", function(){
         total += gemOneVal;
         $("#totalScoreDiv").text(total);
+
+        //if total is more than the random number, increase losses and reset all values
         if(total > randomNum){
             losses++;
             total = 0;
@@ -29,6 +37,7 @@ function clickedGem(){
             $("#totalScoreDiv").text(total);
         }
     
+        //if total is more than the random number, increase wins and reset all values
         if(total == randomNum){
             wins++;
             total = 0;
@@ -42,9 +51,12 @@ function clickedGem(){
         }
     })
 
+    //When gemTwo button is clicked, increase total, update the total display
     $('#gemTwo').on("click", function(){
         total += gemTwoVal;
         $("#totalScoreDiv").text(total);
+
+        //if total is more than the random number, increase losses and reset all values
         if(total > randomNum){
             losses++;
             total = 0;
@@ -57,6 +69,7 @@ function clickedGem(){
             $("#totalScoreDiv").text(total);
         }
     
+        //if total is more than the random number, increase wins and reset all values
         if(total == randomNum){
             wins++;
             total = 0;
@@ -70,9 +83,12 @@ function clickedGem(){
         }
     })
 
+    //When gemThree button is clicked, increase total, update the total display
     $('#gemThree').on("click", function(){
         total += gemThreeVal;
         $("#totalScoreDiv").text(total);
+
+        //if total is more than the random number, increase losses and reset all values
         if(total > randomNum){
             losses++;
             total = 0;
@@ -84,7 +100,8 @@ function clickedGem(){
             displayScore(randomNum);
             $("#totalScoreDiv").text(total);
         }
-    
+
+        //if total is more than the random number, increase wins and reset all values
         if(total == randomNum){
             wins++;
             total = 0;
@@ -98,9 +115,12 @@ function clickedGem(){
         }
     })
 
+    //When gemFouur button is clicked, increase total, update the total display
     $('#gemFour').on("click", function(){
         total += gemFourVal;
         $("#totalScoreDiv").text(total);
+
+        //if total is more than the random number, increase losses and reset all values
         if(total > randomNum){
             losses++;
             total = 0;
@@ -112,7 +132,8 @@ function clickedGem(){
             displayScore(randomNum);
             $("#totalScoreDiv").text(total);
         }
-    
+
+        //if total is more than the random number, increase wins and reset all values
         if(total == randomNum){
             wins++;
             total = 0;
@@ -127,12 +148,14 @@ function clickedGem(){
     })
 }
 
+//function used to display wins, losses, and the current random number
 function displayScore(randomNum){
     $("#randomNumDiv").text("Get this number: " + randomNum);
     $("#winsDiv").text("Wins: " + wins);
     $("#lossesDiv").text("Losses: " + losses);
 }
 
+//function that chooses a random number between a minimum and a maximum
 function randomIntFromInterval(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
